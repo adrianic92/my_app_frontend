deb37a1b759dc722a1792310de4bcc1f0d5ddeae
Adrian <adrianic92@yahoo.com>
2023-03-03 09:21:44 -0800

    added Home component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../App.css';
+import Home from './Home.js'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Hello, it's me...
+      </h1>
+    </div>
+  );
+}
+
+export default App;
