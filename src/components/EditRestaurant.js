7642e0715ee8fd5e74ac247e767706f2508390d6
Adrian <adrianic92@yahoo.com>
2023-03-03 18:30:04 -0800

    added restaurant updater form

--- src/components/EditRestaurant.js
+++ src/components/EditRestaurant.js
@@ -1,7 +1,11 @@
 import React, {useState} from "react";
 
 function EditRestaurant({restaurants}) {
-    const [choice, setChoice] = useState(1)
+    const [id, setId] = useState("")
+    const [name, setName] = useState("")
+    const [description, setDescription] = useState("")
+    const [rating, setRating] = useState("")
+    const [location, setLocation] = useState("")
     
     const allRestaurants = restaurants.map(restaurant => {
         return (
@@ -10,9 +14,39 @@ function EditRestaurant({restaurants}) {
     })
 
     function handleRestaurantChange(e) {
-        setChoice(e.target.value)
+        setId(e.target.value)
+        setName(restaurants[id]).name
+        setDescription(restaurants[id]).description
+        setRating(restaurants[id]).rating
+        setLocation(restaurants[id]).location
     }
 
+    function handleSubmit(e) {
+        e.preventDefault()
+        const updatedRestaurant = {
+           name: name,
+           description: description,
+           rating: rating,
+           location: location
+        }
+        updateRestaurant(updatedRestaurant)
+    }
+
+    function handleNameChange(e) {
+        setName(e.target.value)
+    }
+
+    function handleDescriptionChange(e) {
+        setDescription(e.target.value)
+    }
+    
+    function handleRatingChange(e) {
+        setRating(parseInt(e.target.value))
+    }
+
+    function handleLocationChange(e) {
+        setLocation(e.target.value)
+    }
 
     return (
         <div>
@@ -20,10 +54,12 @@ function EditRestaurant({restaurants}) {
             <form className="form" onSubmit={handleSubmit}>
                 <label>Name:</label>
                 <input type="text" name="name" className='formChild' value={name} onChange={handleNameChange}/>
-                <label>Dessert:</label>
-                <input type="text" name="dessert" className='formChild' value={dessert} onChange={handleDessertChange}/>
-                <select onChange={handleRestaurantChange}>{allRestaurants}</select>
-                <button type="submit" className='submit'>Submit</button>
+                <label>Description:</label>
+                <input type="text" name="description" className='formChild' value={description} onChange={handleDescriptionChange}/>
+                <label>Rating:</label>
+                <input type="number" name="rating" min="1" max="10" className='formChild' value={rating} onChange={handleRatingChange}/>
+                <label>Location:</label>
+                <input type="text" name="location" className='formChild' value={location} onChange={handleLocationChange}/>
             </form>
         </div>
     )
